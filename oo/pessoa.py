6d7b059669b0419b39b6683804a27845f6528f39
Thiago Garcia <thiago-garcia@outlook.com>
2022-09-18 01:05:03 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = 26):
+    def __init__(self, *filhos, nome=None, idade=26):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Felipe')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Thiago'
-    print(p.nome)
-    print(p.idade)
+    thiago = Pessoa(nome='Thiago')
+    luiz = Pessoa(thiago, nome='Luiz')
+    print(Pessoa.cumprimentar(luiz))
+    print(id(luiz))
+    print(luiz.cumprimentar())
+    print(luiz.nome)
+    print(luiz.idade)
+    for filho in luiz.filhos:
+        print(filho.nome)
+    
