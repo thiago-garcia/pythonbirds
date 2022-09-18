3e36c24bdbf7db62777d3d44abf4d4bd25bc5623
Thiago Garcia <thiago-garcia@outlook.com>
2022-09-18 14:37:04 -0300

    Adicionado o objeto Carro

--- oo/carro.py
+++ oo/carro.py
@@ -93,6 +93,29 @@ O       L
     'Oeste'
 """
 
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+    
+    def acelerar(self):
+        self.motor.acelerar()
+    
+    def frear(self):
+        self.motor.frear()
+    
+    def calcular_direcao(self):
+        return self.direcao.valor
+    
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+    
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
 NORTE = 'Norte'
 LESTE = 'Leste'
 SUL = 'Sul'
